60a442a9e4fa31681b62cb909ff5e478e2f5a17a
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-04-21 13:42:04 +1200

    initialised shad-cn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
 import Home from "@pages/Home";
 import Report from "@pages/Report";
 
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -6,6 +6,28 @@
 
 import { Link } from "react-router-dom";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+import { Button } from "@components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/ui/form"
+import { Input } from "@components/ui/input"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
 export default function Home() {
   // const { name } = useParams();
 
@@ -28,6 +50,17 @@ export default function Home() {
   // }
   // return <div>{data}</div>;
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ""
+    }
+  })
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="text-center h-[80vh] pt-24 flex flex-col justify-between items-center">
       <div>
@@ -42,6 +75,28 @@ export default function Home() {
           </button>
         </Link>
       </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }
