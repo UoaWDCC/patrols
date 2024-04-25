a34c8eddbb1b7cc8852772d9b6e9f54f43072c7b
Deasy Lie <dlie042@aucklanduni.ac.nz>
2024-04-25 20:46:07 +1200

    Added changes to the Login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -14,7 +13,7 @@ import { Input } from '@components/ui/input';
 
 export default function Login() {
   const formSchema = z.object({
-    email: z.string().email('Invalid email format').min(2, { message: 'Email must be at least 2 characters' }),
+    email: z.string().email('Invalid email format'),
     password: z.string().min(4, { message: 'Password must be at least 4 characters' }),
   });
   
@@ -31,7 +30,14 @@ export default function Login() {
   };
 
   return (
-    <div>
+    <div className="min-h-screen text-center pt-24 flex flex-col justify-items-center items-center bg-[#eff6ff]">
+      <div>
+        <h3 className="font-bold">One sentence description/opening about the community patrols app</h3>
+      </div>
+      <div>
+        <h2 className="text-2xl tracking-[0.25px] leading-[28.8px] mb-4 mt-20">Log In</h2>
+        <p className="text-xs text-muted-foreground">Please contact XXX if you do not have your login details.</p>
+      </div>
     </div>
   );
 };
