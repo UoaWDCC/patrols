729e20b9ea533c48d176b35888f490ee29e54866
Deasy Lie <dlie042@aucklanduni.ac.nz>
2024-04-25 21:20:55 +1200

    Added changes to the Login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -30,13 +30,53 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen text-center pt-24 flex flex-col justify-items-center items-center bg-[#eff6ff]">
-      <div>
+    <div className="min-h-screen pt-24 flex flex-col justify-items-center items-center bg-[#eff6ff]">
+      <div className='text-center'>
         <h3 className="font-bold">One sentence description/opening about the community patrols app</h3>
       </div>
       <div>
-        <h2 className="text-2xl tracking-[0.25px] leading-[28.8px] mb-4 mt-20">Log In</h2>
+        <h2 className="text-2xl tracking-[0.25px] leading-[28.8px] mb-5 mt-20 text-center">Log In</h2>
         <p className="text-xs text-muted-foreground">Please contact XXX if you do not have your login details.</p>
+      </div>
+      <div className='pt-14'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-base'>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} className='w-80'/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-base'>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className='text-center'>
+            <a href="#" className="text-xs underline text-muted-foreground">
+              Forgot your password?
+            </a>
+          </div>
+          <Button type="submit" className="bg-[#0f1363] w-full flex">
+            Log In
+          </Button>
+        </form>
+      </Form>
       </div>
     </div>
   );
