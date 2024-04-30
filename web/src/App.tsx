5ac3fc62f76769b9ccc557e5bb8e216e7a86512e
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-04-30 18:33:42 +1200

    Added Home component to root url and commented out Auth block

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Home from "@pages/Home";
 import Report from "@pages/Report";
 import LogHome from "@pages/LogHome";
@@ -8,9 +8,12 @@ import Logon from "@pages/Logon";
 import LogonTwo from "@pages/LogonTwo";
 import Login from "@pages/Login";
 import AuthProvider from "./providers/AuthProvider";
-import { useAuth } from "./hooks/useAuth";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/home",
     element: <Home />,
@@ -39,16 +42,16 @@ const router = createBrowserRouter([
 
 export default function App() {
     const [queryClient] = useState(() => new QueryClient());
-    const { user } = useAuth();
-
-    // Render the default page based on user authentication status
-    useEffect(() => {
-        if (!user) {
-          router.navigate("/login"); // Go to login page if user is not authenticated
-        } else {
-          router.navigate("/home"); // Go to home page if user is authenticated
-        }
-    }, []);
+    // const { user } = useAuth();
+  
+    // // Render the default page based on user authentication status
+    // useEffect(() => {
+    //     if (!user) {
+    //       router.navigate("/login"); // Go to login page if user is not authenticated
+    //     } else {
+    //       router.navigate("/home"); // Go to home page if user is authenticated
+    //     }
+    // }, []);
     
     return (
         <QueryClientProvider client={queryClient}>
