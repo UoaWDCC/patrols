2270784d474155a7f6144b0f18fbd049d9eae947
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-05-02 18:46:06 +1200

    fix: Fix components rendering while checking auth status

--- web/src/components/ProtectedRoute.tsx
+++ web/src/components/ProtectedRoute.tsx
@@ -1,18 +1,15 @@
-import { PropsWithChildren, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { PropsWithChildren } from 'react';
+import { Navigate } from 'react-router';
 import { useAuth } from '../hooks/useAuth';
 
 interface IProtectedRoutes extends PropsWithChildren {}
 
 export default function ProtectedRoutes({ children }: IProtectedRoutes) {
     const { user } = useAuth();
-    const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!user) {
-            navigate('/login'), { replace: true }; // Prevents user from navigating back if previously authenticated
-        }
-    }, [navigate, user]);
+    if (!user) {
+        return <Navigate to={'/login'} replace={true} />; // Prevents user from navigating back if previously authenticated
+    }
 
     return children;
 }
