962b417090ed5b749bc78eee9e7c4dff49a63ea1
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-05-02 16:57:42 +1200

    chore: Add usage of ProtectedRoute component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,36 +8,61 @@ import Logon from "@pages/Logon";
 import LogonTwo from "@pages/LogonTwo";
 import Login from "@pages/Login";
 import AuthProvider from "./providers/AuthProvider";
+import ProtectedRoute from '@components/ProtectedRoute';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/LogHome",
-    element: <LogHome />,
-  },
-  {
-    path: "/Report",
-    element: <Report />,
-  },
-  {
-    path: "/logon",
-    element: <Logon />,
-  },
-  {
-    path: "/logon-two",
-    element: <LogonTwo />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
+    {
+        path: '/',
+        element: (
+            <ProtectedRoute>
+                <Home />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/home',
+        element: (
+            <ProtectedRoute>
+                <Home />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/LogHome',
+        element: (
+            <ProtectedRoute>
+                <LogHome />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/Report',
+        element: (
+            <ProtectedRoute>
+                <Report />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/logon',
+        element: (
+            <ProtectedRoute>
+                <Logon />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/logon-two',
+        element: (
+            <ProtectedRoute>
+                <LogonTwo />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
 ]);
 
 export default function App() {
