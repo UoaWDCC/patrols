ee92e6770fa96341132fee4a693260df4c990adb
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-05-05 18:46:40 +1200

    feat: Add updateUserDetails function and route to allow users to update their details

--- api/controller/UserController.ts
+++ api/controller/UserController.ts
@@ -1,6 +1,14 @@
 import prisma from '../db/database';
 import supabase from '../supabase/supabase_client';
 import type { Request, Response } from 'express';
+import { z } from 'zod';
+
+const updateDetailSchema = z.object({
+    id: z.number(),
+    email: z.string(),
+    password: z.string().min(4, 'Password must be at least 4 characters long'),
+    // vehicles: z.string(), // Need to change once we have some vehicle dummy data
+});
 
 export const getUserDetailsByEmail = async (req: Request, res: Response) => {
     try {
@@ -30,5 +38,39 @@ export const getUserDetailsByEmail = async (req: Request, res: Response) => {
         res.status(200).json(userDetails);
     } catch (error) {
         console.error('Error:', error);
+    }
+};
+
+export const updateUserDetails = async (req: Request, res: Response) => {
+    try {
+        const parseResult = updateDetailSchema.safeParse(req.body);
+
+        if (!parseResult.success) {
+            return res.status(400).json({ error: parseResult.error.flatten() });
+        }
+
+        const { id, email, password } = parseResult.data;
+
+        const { error } = await supabase.auth.updateUser({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            console.log(`Error updating details: ${error}`);
+        }
+
+        const updatedUserDetails = await prisma.patrols.update({
+            where: {
+                id: id,
+            },
+            data: {
+                password: password,
+            },
+        });
+
+        res.status(200).json(updatedUserDetails);
+    } catch (error) {
+        console.error('Error: ', error);
     }
 };
--- api/routes/UserRoutes.ts
+++ api/routes/UserRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getUserDetailsByEmail } from '../controller/UserController';
+import {
+    getUserDetailsByEmail,
+    updateUserDetails,
+} from '../controller/UserController';
 
 const userRoutes = Router();
 
 userRoutes.route('/getUserDetails').get(getUserDetailsByEmail);
+userRoutes.route('/updateUserDetails').patch(updateUserDetails);
 
 export default userRoutes;
