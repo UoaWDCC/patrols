0d53a186ebba5a82101c9e98b034cc855b0a269a
syoo881 <syoo881@aucklanduni.ac.nz>
2024-05-14 01:00:41 +1200

    Adding error checking, fixing up design and layout

--- web/src/pages/Logon.tsx
+++ web/src/pages/Logon.tsx
@@ -10,6 +10,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@components/ui/form";
 import cogwheelIcon from "../assets/images/cogwheel.png";
 import userIcon from "../assets/images/gorilla.png";
@@ -19,15 +20,17 @@ export default function Logon() {
 
   const formSchema = z.object({
     shiftTime: z.string(),
-    policeStationBase: z.string(),
-    cpCallSign: z.string(),
-    patrol1Name: z.string(),
-    patrol1Number: z.string(),
+    policeStationBase: z.string().nonempty("Police Station Base is required"),
+    cpCallSign: z.string().nonempty("CP Call Sign is required"),
+    patrol1Name: z.string().nonempty("Patrol 1 Name is required"),
+    patrol1Number: z.string().nonempty("Patrol 1 Mobile Number is required"),
     patrol2Name: z.string(),
     patrol2Number: z.string(),
-    vehicle: z.string(),
-    liveryOrSignage: z.string(),
-    havePoliceRadio: z.string(),
+    vehicle: z.string().refine((value) => value !== "", {
+      message: "Please select a vehicle",
+    }),
+    liveryOrSignage: z.string().nonempty("Livery or Signage is required"),
+    havePoliceRadio: z.string().nonempty("Police Radio is required"),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -54,22 +57,20 @@ export default function Logon() {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="max-w-7xl w-full">
-        <div className="bg-[#EEF6FF] px-8 py-10 flex items-center justify-between">
+        <div className="bg-[#EEF6FF] px-8 py-6 flex items-center justify-between">
           <div className="flex items-center">
-            <img
-              src={userIcon}
-              alt="User Icon"
-              className="w-16 h-16 mr-4 rounded-full"
-            />
+            <img src={userIcon} alt="User Icon" className="w-10 h-10 mr-4" />
             <h2 className="text-2xl font-bold">Welcome back, XXXXXX</h2>
           </div>
-          <div className="flex items-center">
-            <span className="mr-2">Settings</span>
+          <button className="flex items-center">
+            <span className="mr-2 text-lg font-semibold">Settings</span>
             <img src={cogwheelIcon} alt="Settings" className="w-6 h-6" />
-          </div>
+          </button>
         </div>
         <div className="bg-white p-8">
-          <h3 className="text-xl mb-8">Shift Log-on Form</h3>
+          <h3 className="text-3xl font-bold mb-8 text-center">
+            Shift Log-on Form
+          </h3>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="grid grid-cols-2 gap-4">
@@ -80,15 +81,33 @@ export default function Logon() {
                     <FormItem>
                       <FormLabel>Shift Time</FormLabel>
                       <FormControl>
-                        <Input
-                          type="datetime-local"
-                          {...field}
-                          className="w-full"
-                        />
+                        <div className="relative">
+                          <Input
+                            type="datetime-local"
+                            {...field}
+                            className="w-full pl-10"
+                            placeholder="Select date and time"
+                          />
+                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <svg
+                              className="w-5 h-5 text-gray-500"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M6 2C5.44772 2 5 2.44772 5 3V4H4C2.89543 4 2 4.89543 2 6V16C2 17.1046 2.89543 18 4 18H16C17.1046 18 18 17.1046 18 16V6C18 4.89543 17.1046 4 16 4H15V3C15 2.44772 14.5523 2 14 2C13.4477 2 13 2.44772 13 3V4H7V3C7 2.44772 6.55228 2 6 2ZM6 7C5.44772 7 5 7.44772 5 8C5 8.55228 5.44772 9 6 9H14C14.5523 9 15 8.55228 15 8C15 7.44772 14.5523 7 14 7H6Z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          </div>
+                        </div>
                       </FormControl>
                     </FormItem>
                   )}
                 />
+                <div></div>
                 <FormField
                   control={form.control}
                   name="policeStationBase"
@@ -96,8 +115,13 @@ export default function Logon() {
                     <FormItem>
                       <FormLabel>Police Station Base</FormLabel>
                       <FormControl>
-                        <Input {...field} className="w-full" />
+                        <Input
+                          {...field}
+                          className="w-full"
+                          placeholder="Base"
+                        />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -108,13 +132,18 @@ export default function Logon() {
                     <FormItem>
                       <FormLabel>CP Call Sign</FormLabel>
                       <FormControl>
-                        <Input {...field} className="w-full" />
+                        <Input
+                          {...field}
+                          className="w-full"
+                          placeholder="Call Sign"
+                        />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
-                <div className="col-span-2">
-                  <h4 className="text-lg font-semibold mb-2">Patrol 1</h4>
+                <div className="col-span-2 mt-6">
+                  <h4 className="text-2xl font-semibold mb-2">Patrol 1</h4>
                   <div className="grid grid-cols-2 gap-4">
                     <FormField
                       control={form.control}
@@ -123,8 +152,13 @@ export default function Logon() {
                         <FormItem>
                           <FormLabel>Name</FormLabel>
                           <FormControl>
-                            <Input {...field} className="w-full" />
+                            <Input
+                              {...field}
+                              className="w-full"
+                              placeholder="Name"
+                            />
                           </FormControl>
+                          <FormMessage />
                         </FormItem>
                       )}
                     />
@@ -135,15 +169,20 @@ export default function Logon() {
                         <FormItem>
                           <FormLabel>Mobile Number</FormLabel>
                           <FormControl>
-                            <Input {...field} className="w-full" />
+                            <Input
+                              {...field}
+                              className="w-full"
+                              placeholder="Number"
+                            />
                           </FormControl>
+                          <FormMessage />
                         </FormItem>
                       )}
                     />
                   </div>
                 </div>
                 <div className="col-span-2">
-                  <h4 className="text-lg font-semibold mb-2">Patrol 2</h4>
+                  <h4 className="text-2xl font-semibold mb-2">Patrol 2</h4>
                   <div className="grid grid-cols-2 gap-4">
                     <FormField
                       control={form.control}
@@ -152,7 +191,11 @@ export default function Logon() {
                         <FormItem>
                           <FormLabel>Name</FormLabel>
                           <FormControl>
-                            <Input {...field} className="w-full" />
+                            <Input
+                              {...field}
+                              className="w-full"
+                              placeholder="Name"
+                            />
                           </FormControl>
                         </FormItem>
                       )}
@@ -164,41 +207,53 @@ export default function Logon() {
                         <FormItem>
                           <FormLabel>Mobile Number</FormLabel>
                           <FormControl>
-                            <Input {...field} className="w-full" />
+                            <Input
+                              {...field}
+                              className="w-full"
+                              placeholder="Number"
+                            />
                           </FormControl>
                         </FormItem>
                       )}
                     />
                   </div>
                 </div>
-                <div className="col-span-2">
-                  <h4 className="text-lg font-semibold mb-2">Vehicle</h4>
-                  <div className="grid grid-cols-2 gap-4">
+                <div className="col-span-2 mt-6">
+                  <h4 className="text-2xl font-semibold mb-2">Vehicle</h4>
+                  <div className="grid grid-cols-2 gap-4 items-center">
                     <FormField
                       control={form.control}
                       name="vehicle"
                       render={({ field }) => (
                         <FormItem>
-                          <FormLabel>Select an option</FormLabel>
                           <FormControl>
                             <select
                               {...field}
                               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                              onChange={(e) => {
+                                field.onChange(e);
+                              }}
                             >
-                              <option value="">Select a vehicle</option>
+                              <option value="" disabled>
+                                Select an option
+                              </option>
                               <option value="vehicle1">Vehicle 1</option>
                               <option value="vehicle2">Vehicle 2</option>
                             </select>
                           </FormControl>
+                          <FormMessage />
                         </FormItem>
                       )}
                     />
-                    <button
-                      type="button"
-                      className="px-4 py-2 bg-blue-500 text-white rounded-md"
-                    >
-                      Add new vehicle
-                    </button>
+                    <div className="flex items-center justify-center">
+                      <span className="mr-2 font-bold">OR</span>
+                      <button
+                        type="button"
+                        className="px-4 py-2 bg-white text-black border-2 border-[#038400] rounded-md font-semibold underline hover:bg-[#038400]"
+                      >
+                        Add new vehicle
+                      </button>
+                    </div>
                   </div>
                 </div>
                 <FormField
@@ -229,6 +284,7 @@ export default function Logon() {
                           </label>
                         </div>
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -260,12 +316,16 @@ export default function Logon() {
                           </label>
                         </div>
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
               </div>
               <div className="mt-8">
-                <Button type="submit" className="w-full">
+                <Button
+                  type="submit"
+                  className="w-full bg-[#83E960] text-black"
+                >
                   Submit
                 </Button>
               </div>
