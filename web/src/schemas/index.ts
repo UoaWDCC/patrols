2488ede636cf894cc03c655976edf95d12409866
Jason <jasonabc0626@gmail.com>
2024-05-15 12:11:09 +1200

    added emailSchema for resend email

--- web/src/schemas/index.ts
+++ web/src/schemas/index.ts
@@ -6,3 +6,15 @@ export const userDetailsSchema = z.object({
   email: z.string().email(),
   vehicles: z.array(z.string()),
 });
+
+export const tokenSchema = z.object({
+  access_token: z.string(),
+  refresh_token: z.string(),
+});
+
+export const emailSchema = z.object({
+  email: z.string(),
+  patrolName: z.string(),
+  patrolID: z.string(),
+  formData: z.string(),
+})
