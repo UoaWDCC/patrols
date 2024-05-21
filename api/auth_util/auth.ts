09a8042a57e2d18fa914061f09746fb80c7567f7
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-05-21 20:27:05 +1200

    reset

---
+++ api/auth_util/auth.ts
@@ -0,0 +1,34 @@
+import supabase from "../supabase/supabase_client";
+
+interface AuthResult {
+  session?: any;
+  error?: string;
+}
+
+async function authenticateUser(
+  email: string,
+  password: string
+): Promise<AuthResult> {
+  try {
+    // Authenticate against Supabase
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      return { error: error.message };
+    }
+
+    if (data && data.session) {
+      return { session: data.session };
+    }
+    console.log(data.session);
+
+    return { error: "Authentication failed, no session available." };
+  } catch (error: any) {
+    return { error: error.message || "Failed to authenticate" };
+  }
+}
+
+export { authenticateUser };
