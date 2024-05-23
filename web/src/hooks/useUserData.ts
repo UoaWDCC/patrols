bdbe7e516ae1de14958dbc633ebbecf6a6db6884
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-05-23 23:11:03 +1200

    feat: Add caching for user data to reduce calls to api

--- web/src/hooks/useUserData.ts
+++ web/src/hooks/useUserData.ts
@@ -1,21 +1,44 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { z } from "zod";
-import { userDetailsSchema, vehicleDetailsSchema } from "../schemas";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { z } from 'zod';
+import { useQuery } from '@tanstack/react-query';
+import {
+  userDetailsSchema,
+  patrolDetailsSchema,
+  vehicleDetailsSchema,
+} from '../schemas';
 
 type UserDetails = z.infer<typeof userDetailsSchema>;
 type VehicleDetails = z.infer<typeof vehicleDetailsSchema>;
 
+const fetchUserData = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/user/getUserDetails`
+    );
+    const { userDetails, patrolDetails, vehicleDetails } = response.data;
+    const parsedUserDetails = userDetailsSchema.parse(userDetails);
+    const parsedPatrolDetails = patrolDetailsSchema.parse(patrolDetails);
+    const parsedVehicleDetails = vehicleDetailsSchema
+      .array()
+      .parse(vehicleDetails);
+    console.log('API CALLED');
+    return { parsedUserDetails, parsedPatrolDetails, parsedVehicleDetails };
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
 const useUserData = () => {
   const [loading, setLoading] = useState(true);
   const [currentUserDetails, setCurrentUserDetails] = useState<UserDetails>();
-  const [fullName, setFullName] = useState<string>("");
-  const [cpnzID, setCPNZID] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [mobileNumber, setMobileNumber] = useState<string>("");
-  const [callSign, setCallSign] = useState<string>("");
-  const [patrolName, setPatrolName] = useState<string>("");
-  const [policeStation, setPoliceStation] = useState<string>("");
+  const [fullName, setFullName] = useState<string>('');
+  const [cpnzID, setCPNZID] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [mobileNumber, setMobileNumber] = useState<string>('');
+  const [callSign, setCallSign] = useState<string>('');
+  const [patrolName, setPatrolName] = useState<string>('');
+  const [policeStation, setPoliceStation] = useState<string>('');
   const [currentUserVehicles, setCurrentUserVehicles] = useState<
     VehicleDetails[]
   >([]);
@@ -24,49 +47,45 @@ const useUserData = () => {
   );
   const [membersInPatrol, setMembersInPatrol] = useState<UserDetails[]>([]);
 
+  const { data } = useQuery({
+    queryKey: ['userData'],
+    queryFn: fetchUserData,
+    staleTime: 300000,
+  });
+
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_API_URL}/user/getUserDetails`
+    if (data) {
+      const { parsedUserDetails, parsedPatrolDetails, parsedVehicleDetails } =
+        data;
+
+      setCurrentUserDetails(parsedUserDetails);
+      setCallSign(parsedUserDetails.call_sign);
+      setPatrolName(parsedPatrolDetails.name);
+      setPoliceStation(parsedUserDetails.police_station.replace(/_/g, ' '));
+      setEmail(parsedUserDetails.email);
+      setCPNZID(parsedUserDetails.cpnz_id);
+      setMobileNumber(parsedUserDetails.mobile_phone);
+      setFullName(
+        `${parsedUserDetails.first_names} ${parsedUserDetails.surname}`
+      );
+
+      if (parsedVehicleDetails.length === 0) {
+        setSelectedVehicle(null);
+        setCurrentUserVehicles([]);
+      } else {
+        setSelectedVehicle(
+          parsedVehicleDetails.find((vehicle) => vehicle.selected) || null
         );
-
-        const { userDetails, patrolDetails, vehicleDetails } = response.data;
-        const parsedUserDetails = userDetailsSchema.parse(userDetails);
-        const parsedVehicleDetails = vehicleDetailsSchema
-          .array()
-          .parse(vehicleDetails);
-        setCurrentUserDetails(parsedUserDetails);
-        setCallSign(userDetails.call_sign);
-        setPatrolName(patrolDetails.name);
-        setPoliceStation(userDetails.police_station.replace(/_/g, " ")); // Replace enum underscores with space
-        setEmail(userDetails.email);
-        setCPNZID(userDetails.cpnz_id);
-        setMobileNumber(userDetails.mobile_phone);
-        setFullName(`${userDetails.first_names} ${userDetails.surname}`);
-
-        if (parsedVehicleDetails.length === 0) {
-          setSelectedVehicle(null);
-          setCurrentUserVehicles([]);
-        } else {
-          setSelectedVehicle(
-            parsedVehicleDetails.find((vehicle) => vehicle.selected) || null
-          );
-          const reorderedVehicles = [
-            ...parsedVehicleDetails.filter((vehicle) => vehicle.selected),
-            ...parsedVehicleDetails.filter((vehicle) => !vehicle.selected),
-          ];
-          setCurrentUserVehicles(reorderedVehicles);
-        }
-        setMembersInPatrol(patrolDetails.members_dev);
-        setLoading(false);
-      } catch (e) {
-        console.log("Error: ", e);
+        const reorderedVehicles = [
+          ...parsedVehicleDetails.filter((vehicle) => vehicle.selected),
+          ...parsedVehicleDetails.filter((vehicle) => !vehicle.selected),
+        ];
+        setCurrentUserVehicles(reorderedVehicles);
       }
-    };
-
-    fetchUserData();
-  }, []);
+      setMembersInPatrol(parsedPatrolDetails.members_dev);
+      setLoading(false);
+    }
+  }, [data]);
 
   return {
     loading,
