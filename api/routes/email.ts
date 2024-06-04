4980b0c8a9799086455f2c0a2fc6796e9a6c01ac
Todaricli <leetony347@yahoo.com>
2024-06-04 13:55:42 +1200

    add email history route, renamed file

--- api/routes/email.ts
+++ api/routes/email.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { sendEmail } from '../controller/emailController';
-import { getMails, getUser, watchMails } from '../controller/gmailController';
+import { getMails, getUser, watchMails, getHistoryRecords } from '../controller/gmailController';
 
 const emailRoute = Router();
 
 emailRoute.route('/sendEmail').post(sendEmail);
-emailRoute.route('/fetchGmailThread').get(getMails)
+emailRoute.route('/fetchMessage').get(getMails)
 emailRoute.route('/fetchGmailUser').get(getUser)
 emailRoute.route('/monitorEmails').post(watchMails) // not working yet
+emailRoute.route('/checkNewEmails').get(getHistoryRecords)
 
 export default emailRoute;
