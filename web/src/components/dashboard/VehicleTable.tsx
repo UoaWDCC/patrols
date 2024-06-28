48d6213ed4cb39c1aa742e1cfd3a5f41414724a4
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-06-28 19:11:52 +1200

    style: Remove useless import

--- web/src/components/dashboard/VehicleTable.tsx
+++ web/src/components/dashboard/VehicleTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { userDetailsSchema, vehicleDetailsSchema } from '../../schemas';
 import { z } from 'zod';
