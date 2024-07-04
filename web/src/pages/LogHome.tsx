0b8db14f024ac9dbcc6975f44ab33343228832b6
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-07-04 15:59:41 +1200

    Removed button that leads to confirmation

--- web/src/pages/LogHome.tsx
+++ web/src/pages/LogHome.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import BottomNavBar from "@components/BottomNavBar";
 import LargeInfoButton from "@components/ui/LargeInfoButton";
 import useDraftStatus from "../hooks/useDraftStatus";
+import useUserData from "../hooks/useUserData";
+import { useEffect } from "react";
 
 export default function ReportSummary() {
   const navigate = useNavigate();
@@ -16,9 +18,14 @@ export default function ReportSummary() {
     navigate("/report");
   };
 
-  const handleConfirmation = () => {
-    navigate("/confirmation");
-  }
+  const { currentUserDetails } = useUserData();
+
+  // Check the curent user's logon status, if "Yes", then redirect to logon home
+  useEffect(() => {
+    if (currentUserDetails && currentUserDetails.logon_status != "Yes") {
+      navigate("/home");
+    }
+  }, [currentUserDetails?.logon_status]);
 
   return (
     <div className="text-center min-h-screen relative bg-[#FFFFFF] max-w-3xl mx-auto">
@@ -32,28 +39,27 @@ export default function ReportSummary() {
       <div className="max-w-800 mx-auto px-14 my-8">
         <div>
           <div className="py-12">
-            <h2 className="text-base font-bold">
-              Patrol in progress.
-            </h2>
-            <h3 className="font-light">
-              Event ID: xxxxxxxxxx
-            </h3>
+            <h2 className="text-base font-bold">Patrol in progress.</h2>
+            <h3 className="font-light">Event ID: xxxxxxxxxx</h3>
           </div>
           {isDraft && (
-          <LargeInfoButton
-            heading={"Draft report detected"}
-            description={"You have a report you haven't submitted."}
-            className="bg-[#0F1363] p-4 rounded-lg shadow-md mb-6"
-            iconDescription={"Finish your report?"}
-            onClick={handleDraftReport}
-            variant={"dark"}
-          />
-        )}
-          <button onClick={handleNewReport} className="w-full">
+            <LargeInfoButton
+              heading={"Draft report detected"}
+              description={"You have a report you haven't submitted."}
+              className="bg-[#0F1363] p-4 rounded-lg shadow-md mb-6"
+              iconDescription={"Finish your report?"}
+              onClick={handleDraftReport}
+              variant={"dark"}
+            />
+          )}
+          <button
+            onClick={handleNewReport}
+            className="w-full transition-all duration-300 hover:opacity-80"
+          >
             <div className="bg-cpnz-blue-900 rounded-lg shadow-md mb-6 py-14 items-center flex justify-center w-full">
-                <h1 className="text-white font-semibold text-lg">
-                  Start a New Report
-                </h1>
+              <h1 className="text-white font-semibold text-lg">
+                Start a New Report
+              </h1>
             </div>
           </button>
         </div>
@@ -68,13 +74,6 @@ export default function ReportSummary() {
             <p className="text-xs">Details incorrect?</p>
             <button className="bg-[#EEF6FF] rounded-lg py-2 px-4 shadow-md">
               Make Amendment
-            </button>
-          </div>
-          <div>
-            <button className="bg-[#FF8080] my-10 rounded-lg shadow-md p-4 w-full hover:bg-[#ff4d4d]"
-            onClick={handleConfirmation}
-            >
-              Submit Report & Log Off
             </button>
           </div>
         </div>
