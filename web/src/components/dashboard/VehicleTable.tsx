e198d9798f97e96494086c1ac7b3d842ddc044b6
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-07-10 18:39:07 +1200

    rewrote current table using shadcn table

--- web/src/components/dashboard/VehicleTable.tsx
+++ web/src/components/dashboard/VehicleTable.tsx
@@ -1,127 +1,151 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { userDetailsSchema, vehicleDetailsSchema } from '../../schemas';
-import { z } from 'zod';
-import AddVehicleModal from './AddVehicleModal'; // Adjust the import path as needed
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { userDetailsSchema, vehicleDetailsSchema } from "../../schemas";
+import { z } from "zod";
+import AddVehicleModal from "./AddVehicleModal"; // Adjust the import path as needed
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@components/ui/table";
+import { Button } from "@components/ui/button";
 
 type VehicleDetails = z.infer<typeof vehicleDetailsSchema>;
 
 const VehicleTable = () => {
-    const [patrolId, setPatrolId] = useState<string>('');
-    const [vehicleData, setVehicleData] = useState<VehicleDetails[]>([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [patrolId, setPatrolId] = useState<string>("");
+  const [vehicleData, setVehicleData] = useState<VehicleDetails[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        const getPatrolLeadID = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_API_URL}/user/getUserDetails`);
-                const userDetails = userDetailsSchema.parse(response.data.userDetails);
-                setPatrolId(String(userDetails.patrol_id));
-            } catch (error) {
-                console.error('Error fetching patrol lead ID:', error);
-            }
-        };
-
-        getPatrolLeadID();
-    }, []);
-
-    useEffect(() => {
-        const getVehicleByPatrolId = async () => {
-            if (!patrolId) return;
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_API_URL}/vehicle/${patrolId}`);
-                const vehicleData = vehicleDetailsSchema.array().parse(response.data);
-                setVehicleData(vehicleData);
-            } catch (error) {
-                console.error('Error fetching vehicle data:', error);
-            }
-        };
-
-        getVehicleByPatrolId();
-    }, [patrolId]);
-
-    const handleAddVehicle = async (newVehicle: VehicleDetails) => {
-        try {
-            const response = await axios.post(`${import.meta.env.VITE_API_URL}/vehicle`, newVehicle);
-            const addedVehicle = response.data;
-            setVehicleData([...vehicleData, addedVehicle]);
-            setIsModalOpen(false); 
-        } catch (error) {
-            console.error('Error adding vehicle:', error);
-        }
+  useEffect(() => {
+    const getPatrolLeadID = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/user/getUserDetails`
+        );
+        const userDetails = userDetailsSchema.parse(response.data.userDetails);
+        setPatrolId(String(userDetails.patrol_id));
+      } catch (error) {
+        console.error("Error fetching patrol lead ID:", error);
+      }
     };
 
-    const removeVehicle = async (id: string) => {
-        try {
-            await axios.delete(`${import.meta.env.VITE_API_URL}/vehicle/${id}`);
-            setVehicleData(prev => prev.filter(vehicle => vehicle.id !== id));
-        } catch (error) {
-            console.error('Error deleting vehicle:', error);
-        }
+    getPatrolLeadID();
+  }, []);
+
+  useEffect(() => {
+    const getVehicleByPatrolId = async () => {
+      if (!patrolId) return;
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/vehicle/${patrolId}`
+        );
+        const vehicleData = vehicleDetailsSchema.array().parse(response.data);
+        setVehicleData(vehicleData);
+      } catch (error) {
+        console.error("Error fetching vehicle data:", error);
+      }
     };
-    
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+    getVehicleByPatrolId();
+  }, [patrolId]);
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const handleAddVehicle = async (newVehicle: VehicleDetails) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/vehicle`,
+        newVehicle
+      );
+      const addedVehicle = response.data;
+      setVehicleData([...vehicleData, addedVehicle]);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error("Error adding vehicle:", error);
+    }
+  };
 
-    return (
-        <div className="container mx-auto">
-            <h2 className="text-center text-2xl font-bold my-4">Vehicle List</h2>
-            <button
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4"
-                onClick={openModal}
-            >
-                Add Vehicle
-            </button>
-            <table className="table-auto w-full border-collapse border border-gray-400">
-                <thead>
-                    <tr className="bg-gray-200">
-                        <th className="border border-gray-400 px-4 py-2">Registration No</th>
-                        <th className="border border-gray-400 px-4 py-2">Colour</th>
-                        <th className="border border-gray-400 px-4 py-2">Model</th>
-                        <th className="border border-gray-400 px-4 py-2">Make</th>
-                        <th className="border border-gray-400 px-4 py-2">Has Livery or Signage</th>
-                        <th className="border border-gray-400 px-4 py-2">Has Police Radio</th>
-                        <th className="border border-gray-400 px-4 py-2">Selected</th>
-                        <th className="border border-gray-400 px-4 py-2">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {vehicleData.map(vehicle => (
-                        <tr key={vehicle.id}>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.registration_no}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.colour}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.model}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.make}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.has_livery_or_signage ? 'Yes' : 'No'}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.has_police_radio ? 'Yes' : 'No'}</td>
-                            <td className="border border-gray-400 px-4 py-2">{vehicle.selected ? 'Yes' : 'No'}</td>
-                            <td className="border border-gray-400 px-4 py-2">
-                                <button
-                                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
-                                    onClick={() => removeVehicle(vehicle.id)}
-                                >
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+  const removeVehicle = async (id: string) => {
+    try {
+      await axios.delete(`${import.meta.env.VITE_API_URL}/vehicle/${id}`);
+      setVehicleData((prev) => prev.filter((vehicle) => vehicle.id !== id));
+    } catch (error) {
+      console.error("Error deleting vehicle:", error);
+    }
+  };
 
-            <AddVehicleModal
-                isOpen={isModalOpen}
-                onClose={closeModal}
-                onAddVehicle={handleAddVehicle}
-                patrolId={patrolId}
-            />
-        </div>
-    );
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-center text-2xl font-bold mt-8">Vehicle List</h2>
+      <div className="p-12 shadow-md mb-8">
+        <Table>
+          <TableCaption>
+            List of all vehicles registered in your patrol.
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Registration No</TableHead>
+              <TableHead>Colour</TableHead>
+              <TableHead>Model</TableHead>
+              <TableHead>Make</TableHead>
+              <TableHead>Has Livery or Signage</TableHead>
+              <TableHead>Has Police Radio</TableHead>
+              <TableHead>Has Police selected</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {vehicleData.map((vehicle) => (
+              <TableRow key={vehicle.id}>
+                <TableCell className="font-medium">
+                  {vehicle.registration_no}
+                </TableCell>
+                <TableCell>{vehicle.colour}</TableCell>
+                <TableCell>{vehicle.model}</TableCell>
+                <TableCell>{vehicle.make}</TableCell>
+                <TableCell>
+                  {vehicle.has_livery_or_signage ? "Yes" : "No"}
+                </TableCell>
+                <TableCell>{vehicle.has_police_radio ? "Yes" : "No"}</TableCell>
+                <TableCell>{vehicle.selected ? "Yes" : "No"}</TableCell>
+                <TableCell>
+                  <Button
+                    variant={"destructive"}
+                    className="text-[14px]"
+                    onClick={() => removeVehicle(vehicle.id)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <Button className="bg-cpnz-blue-800" onClick={openModal}>
+        Add Vehicle
+      </Button>
+
+      <AddVehicleModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onAddVehicle={handleAddVehicle}
+        patrolId={patrolId}
+      />
+    </div>
+  );
 };
 
 export default VehicleTable;
