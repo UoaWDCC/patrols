406a9d04cc0e617253fd0a4b2224154eaefd7875
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-07-11 16:09:30 +1200

    Update LogOff.ts

--- api/routes/LogOff.ts
+++ api/routes/LogOff.ts
@@ -1,9 +1,8 @@
-import { Router } from 'express';
-import { logOffStatus, logOffEmail } from '../controller/LogOffController';
+import { Router } from "express";
+import { logOffEmail } from "../controller/LogOffController";
 
 const logOffRoutes = Router();
 
-logOffRoutes.route('/:id').patch(logOffStatus)
-logOffRoutes.route('/').post(logOffEmail)
+logOffRoutes.route("/").post(logOffEmail);
 
 export default logOffRoutes;
