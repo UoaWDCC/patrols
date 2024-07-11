668d2d6fa9294c56db87ab1a3c1b1ea8e596967d
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-07-11 19:23:36 +1200

    made navbar sticky

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import BottomNavBar from "@components/BottomNavBar";
 import LocationOfInterestTable from "@components/dashboard/LocationOfInterestTable";
 import Reports from "@components/dashboard/Reports";
 import VehicleTable from "@components/dashboard/VehicleTable";
@@ -8,6 +9,8 @@ const Dashboard = () => {
       <VehicleTable />
       <LocationOfInterestTable showActions={true} />
       <Reports />
+
+      <BottomNavBar />
     </div>
   );
 };
