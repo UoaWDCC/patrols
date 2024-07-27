94c03e8ef8f8932ea01c1de125248111ea96c64e
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-07-27 19:48:52 +1200

    stash

---
+++ api/controller/stolenVehicleController.ts
@@ -0,0 +1,58 @@
+import type { Response, Request } from "express";
+import prisma from "../db/database";
+
+function toObject(report: any) {
+  return JSON.parse(
+    JSON.stringify(report, (key, value) =>
+      typeof value === "bigint" ? value.toString() : value
+    )
+  );
+}
+
+
+export const getAllStolenVehicle = async (req: Request, res: Response) => {
+  try {
+    const stolenVehicles = await prisma.stolen_vehicle.findMany({
+      select: {
+        registration_number : true,
+        vehicle_color : true,
+        vehicle_brand : true,
+        vehicle_model : true,
+        vehicle_type : true,
+        years : true,
+        reported_date : true,
+        city : true,
+      }
+    });
+    if (!stolenVehicles || stolenVehicles.length === 0) {
+      return res.status(404).json({ error: "There are no stolen vehicles" });
+    }
+    return res.status(200).json(toObject(stolenVehicles));
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message});
+  }
+}
+
+export const getSingleStolenVehicle = async (req: Request, res: Response) => {
+  const {registration_no} = req.params
+  try {
+    const stolenVehicle = await prisma.stolen_vehicle.findUnique({
+      where: { registration_number: registration_no },
+      select: {
+        vehicle_color : true,
+        vehicle_brand : true,
+        vehicle_model : true,
+        vehicle_type : true,
+        years : true,
+        reported_date : true,
+        city : true,
+      }
+    });
+    if (!stolenVehicle) {
+      return res.status(404).json({ error: "Stolen vehicle not found" });
+    }
+    return res.status(200).json(toObject(stolenVehicle));
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
+}
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -15,46 +15,13 @@ import logon from "../assets/images/logon.png";
 
 import useUserData from "../hooks/useUserData";
 
-// const reportsDetailsSchema = z.object({
-//   message: z.string(),
-//   reports: z.array(
-//     z.object({
-//       id: z.number(),
-//       title: z.string(),
-//       createdAt: z.string(),
-//       location: z.string(),
-//       patrolID: z.number(),
-//       reportIncidentType: z.string(),
-//     })
-//   ),
-// });
-
-// type reportsDetails = z.infer<typeof reportsDetailsSchema>;
-
 export default function Home() {
   const navigate = useNavigate();
-  // const [data, setData] = useState<reportsDetails>();
-  // const [id, setId] = useState<number>();
 
   // Function to navigate to the logon page when new report button is clicked
   const handleNewReport = () => {
     navigate("/logon");
   };
-
-  // useEffect(() => {
-  //   const getAllReports = async () => {
-  //     const response = await axios.get(
-  //       `${import.meta.env.VITE_API_URL}/report/lead/${id}`
-  //     );
-
-  //     const reportsData = reportsDetailsSchema.parse(response.data);
-  //     setData(reportsData);
-  //   };
-
-  //   if (id !== undefined) {
-  //     getAllReports();
-  //   }
-  // }, [id]);
 
   const { currentUserDetails } = useUserData();
 
@@ -101,45 +68,7 @@ export default function Home() {
         </div>
         <div className="mb-10">
           <h2 className="font-bold text-left">Past Reports</h2>
-          <div className="p-10 bg-[#F8F8F8] shadow-md mt-4">
-            <Dialog>
-              <DialogTrigger className="flex-1 text-black p-4"></DialogTrigger>
-              <DialogContent className="p-8 max-h-[550px] overflow-y-auto">
-                <DialogHeader>
-                  <DialogTitle className="text-center text-subheading pb-12">
-                    All Reports
-                  </DialogTitle>
-                  <DialogDescription>
-                    {/* {data == null ? (
-                      <span>No reports found</span>
-                    ) : (
-                      <div className="flex flex-col w-full gap-8">
-                        {data.reports.map((d) => (
-                          <div
-                            key={d.id}
-                            className="flex-1 border-2 border-zinc-400 rounded-lg shadow-md px-6 py-4 hover:bg-zinc-200 transition-all cursor-pointer"
-                          >
-                            <h3 className="text-lg font-semibold text-cpnz-blue-900 ">
-                              {d.title}
-                            </h3>
-                            <p>
-                              <strong>Location</strong>: {d.location}
-                            </p>
-                            <p>
-                              <strong>Type:</strong> {d.reportIncidentType}
-                            </p>
-                            <p>
-                              <strong>Patrol ID:</strong> {d.patrolID}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
-                    )} */}
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </div>
+          <div className="p-10 bg-[#F8F8F8] shadow-md mt-4"></div>
 
           <button className="w-full">
             <div className="p-4 bg-[#EEF6FF] shadow-md mt-4">
@@ -149,6 +78,20 @@ export default function Home() {
         </div>
       </div>
       <BottomNavBar />
+
+      <Dialog>
+        <DialogTrigger className="flex-1 text-black p-4"></DialogTrigger>
+        <DialogContent className="p-8 max-h-[550px] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle className="text-center text-subheading pb-12">
+              All Reports
+            </DialogTitle>
+            <DialogDescription>
+              Here are all your past reports
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
